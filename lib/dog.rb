c5df6fbc597379b09a226022d6788ef7f01246d1
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-28 23:49:33 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog 
+  attr_accessor :name, :breed, :id 
+  
+  def initialize(id: nil, name:, breed:)
+    @id, @name, @breed = id, name, breed
+  end 
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        breed TEXT
+        );
+      SQL
+      
+      DB[:conn].execute(sql)
+  end 
+  
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs;")
+  end 
+  
+  def save 
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?,?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+  end 
+  
+  def self.create(attributes)
+    dog = Dog.new(attributes)
+    dog.save
+    dog
+  end 
+  
+end 
