31406a5b06fb625bd856971c7f7b511836448608
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-29 00:50:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,9 +52,8 @@ class Dog
       WHERE id = ?
       SQL
       
-    DB[:conn].execute(sql, id).map do |row|
-      self.new_from_db(row)
-    end
+    dog = DB[:conn].execute(sql, id).flatten
+    new_from_db(dog)
   end 
   
   def self.find_or_create_by(name:, breed:)
